b2c2e3e4bb1d44f9ce1aa19cbf6c18ceb35fd609
Catalin <catalin.es@hotmail.com>
2024-06-06 17:18:33 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+function imagePaletteItems() {
+    for (const photo of galleryItems) {
+        //creare tag img + atribute/clasa
+        const image = document.createElement("img");
+        image.classList.add("gallery__image");
+        image.setAttribute("src", photo.preview);
+        image.setAttribute("data-source", photo.original);
+        image.setAttribute("alt", photo.description);
+
+        //creare tag li + atribute/clasa
+        const elem = document.createElement("li");
+        elem.classList.add("gallery__item");
+
+        //creare tag a + atribute/clasa
+        const link = document.createElement("a");
+        link.setAttribute("href", photo.original);
+        link.classList.add("gallery__link");
+  
+        //aranjare elemente
+        elem.appendChild(link);
+        link.appendChild(image);
+        list.appendChild(elem);
+
+        //Dezactivare comportament redirectionare/download
+        link.addEventListener("click", (event) => {
+        event.preventDefault();
+        });
+        
+        let instance;
+        //deschidere fereastra modala cu imaginea completa
+        image.addEventListener("click", () => {
+        instance = basicLightbox.create(`<img width="800" height="600" src="${photo.original}">`)
+        instance.show();
+        }); 
+        
+         //inchidere fereastra modala cu imaginea completa  
+        document.addEventListener("keyup", (event) => {
+            if (event.code === "Escape") {
+                instance.close();
+            }
+        })
+       
+        
+    }
+ 
+}
+
+imagePaletteItems();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+function imagePaletteItems() {
+    const list = document.querySelector(".gallery");
+    for (const photo of galleryItems) {
+        // reare tag img + atribute/clasa
+        const image = document.createElement("img");
+        image.classList.add("gallery__image");
+        image.setAttribute("src", photo.preview);
+        image.setAttribute("alt", photo.description);
+
+        // creare tag li + atribute/clasa
+        const elem = document.createElement("li");
+        elem.classList.add("gallery__item");
+
+        // creare tag a + atribute/clasa
+        const link = document.createElement("a");
+        link.setAttribute("href", photo.original);
+        link.classList.add("gallery__link");
+  
+        // aranjare elemente
+        elem.appendChild(link);
+        link.appendChild(image);    
+        list.appendChild(elem);
+
+        // Dezactivare comportament redirectionare/download
+        link.addEventListener("click", (event) => {
+        event.preventDefault();
+        });
+       
+        
+    }
+        // Animatie imagini 
+        const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+ 
+}
+
+imagePaletteItems();
 console.log(galleryItems);
